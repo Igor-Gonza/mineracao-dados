32a64bbd55230eb04f2e9db4e09d1bcaef38c1c6
Igor Gonzalez <igorgustavogonza@gmail.com>
2024-02-28 10:41:37 -0300

    Primeiros arquivos

---
+++ app.py
@@ -0,0 +1,14 @@
+
+idade = 20
+
+print(f"Idade: {idade}")
+
+soma = idade + 5
+subtracao = idade - 5
+multiplicacao = idade * 3
+divisao = idade / 5
+
+print(f"Soma: {soma}")
+print(f"Subtração: {subtracao}")
+print(f"Multiplicação: {multiplicacao}")
+print(f"Divisão {divisao}")
